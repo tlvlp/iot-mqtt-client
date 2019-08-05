72f29f5035c43bb8fda749ca26bc4720ee2fed48
Peter Veres <tllvllp@gmail.com>
2019-08-05 18:59:53 +0200

    Optimized MQTT Broker connection, using only the built-in option
    Signed-off-by: Peter Veres <tllvllp@gmail.com>

--- src/main/java/com/tlvlp/iot/server/mqtt/client/mqtt/BrokerConnector.java
+++ src/main/java/com/tlvlp/iot/server/mqtt/client/mqtt/BrokerConnector.java
@@ -6,23 +6,18 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BrokerConnector {
 
     private static final Logger log = LoggerFactory.getLogger(BrokerConnector.class);
-    private Properties properties;
     private MqttClient client;
     private MqttConnectOptions connectOptions;
-    private Boolean connectionInProgress;
 
 
     public BrokerConnector(MqttClient client, Properties properties) {
         this.client = client;
-        this.properties = properties;
-        connectionInProgress = false;
         connectOptions = new MqttConnectOptions();
         connectOptions.setUserName(properties.MQTT_CLIENT_MQTT_BROKER_USER);
         connectOptions.setPassword(properties.MQTT_CLIENT_MQTT_BROKER_PASS_SECRET_FILE_PARSED.toCharArray());
@@ -32,23 +27,11 @@ public class BrokerConnector {
         connectOptions.setCleanSession(false);
     }
 
-    //TODO - IS THE SCHEDULED CHECKER NECESSARY?
-    @Scheduled(fixedDelayString = "${MQTT_CLIENT_CONNECTION_CHECK_MS}", initialDelay = 5000)
-    public void checkConnection() {
-        if (!client.isConnected() && !connectionInProgress) {
-            log.warn("Connection lost to MQTT broker!");
-            connectToBroker();
-            //TODO - does it automatically resubscribe to topics?
-        }
-    }
-
     public void connectToBroker() {
-        connectionInProgress = true;
         log.info("Connecting to MQTT broker");
         while (!client.isConnected()) {
             try {
                 client.connect(connectOptions);
-                connectionInProgress = false;
                 log.info("Connected to MQTT broker with client ID: " + client.getClientId());
             } catch (MqttException e) {
                 log.debug("Error connecting to MQTT broker: ", e);
@@ -60,5 +43,4 @@ public class BrokerConnector {
             }
         }
     }
-
 }
