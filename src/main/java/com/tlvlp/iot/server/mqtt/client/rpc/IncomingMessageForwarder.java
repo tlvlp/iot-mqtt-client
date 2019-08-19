4968b338400acfbda24c665a52bc5b978fa222a5
Peter Veres <tllvllp@gmail.com>
2019-08-19 03:57:43 +0200

    Message Forwarder is now fully functional
    Signed-off-by: Peter Veres <tllvllp@gmail.com>

--- src/main/java/com/tlvlp/iot/server/mqtt/client/rpc/IncomingMessageForwarder.java
+++ src/main/java/com/tlvlp/iot/server/mqtt/client/rpc/IncomingMessageForwarder.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,18 +25,15 @@ public class IncomingMessageForwarder {
 
     public void forwardMessage(Message message) {
         try {
-            ResponseEntity response = restTemplate.postForEntity(
-                    String.format("http://%s", properties.UNIT_SERVICE_MESSAGE_RESOURCE_URI),
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    properties.UNIT_SERVICE_MESSAGE_RESOURCE_URI,
                     message,
-                    ResponseEntity.class);
-            if (response.getStatusCodeValue() == 202) {
-                log.debug("Message (id: {}) forwarded to the Unit Service.", message.getTimeArrived());
-            } else {
-                log.warn("Message (id: {}) could not be forwarded to Unit Service: {} {}",
-                        message.getTimeArrived(), response.getStatusCode(), response.getBody());
-            }
+                    String.class);
+            log.debug("Message (id: {}) forwarded to the Unit Service.", message.getTimeArrived());
         } catch (ResourceAccessException e) {
-            log.error("Error! Unit Service is not responding: {}", e.getMessage());
+            log.error("Unit Service is not responding: {}", e.getMessage());
+        } catch (HttpServerErrorException | HttpClientErrorException e) {
+            log.error("Cannot forward message to Unit Service: {}", e.getResponseBodyAsString());
         }
     }
 
