4867f4790f2faeaf130a13e30969cc6c62a98d37
Peter Veres <tllvllp@gmail.com>
2019-09-26 12:08:02 +0200

    Updates SecretsLoader not to exit the application when the secret env variables are not present. It was necessary as MockMVC was running the EnvrionmentPostProcessor and with it the SecretsLoader.
    Signed-off-by: Peter Veres <tllvllp@gmail.com>

--- src/main/java/com/tlvlp/iot/server/mqtt/client/config/SecretsLoader.java
+++ src/main/java/com/tlvlp/iot/server/mqtt/client/config/SecretsLoader.java
@@ -45,24 +45,29 @@ public class SecretsLoader implements EnvironmentPostProcessor {
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        System.out.printf("%n%nDiscovering and loading file-based secrets before the service starts.%n");
-        PropertySource<?> system = environment.getPropertySources().get(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME);
-        Map<String, Object> allEnvVariables = environment.getSystemEnvironment();
+        try {
+            System.out.printf("%n%nDiscovering and loading file-based secrets before the service starts.%n");
+            PropertySource<?> system = environment.getPropertySources().get(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME);
+            Map<String, Object> allEnvVariables = environment.getSystemEnvironment();
 
-        System.out.println("Searching for secrets folder environment variable:");
-        String secretsFolderName = findFolderVar(allEnvVariables);
-        String secretsFolder = getEnvValue(secretsFolderName, system);
+            System.out.println("Searching for secrets folder environment variable:");
+            String secretsFolderName = findFolderVar(allEnvVariables);
+            String secretsFolder = getEnvValue(secretsFolderName, system);
 
-        System.out.println("Searching for secret file environment variables:");
-        List<String> secretFileEnvVariables = findFileVars(allEnvVariables);
-        Map<String, String> secretFileNames = getSecretFileNames(secretFileEnvVariables, system);
+            System.out.println("Searching for secret file environment variables:");
+            List<String> secretFileEnvVariables = findFileVars(allEnvVariables);
+            Map<String, String> secretFileNames = getSecretFileNames(secretFileEnvVariables, system);
 
-        System.out.println("Reading secret files:");
-        Map<String, Object> secrets = getSecrets(secretFileNames, secretsFolder);
-        environment.getPropertySources()
-                .addAfter(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, new MapPropertySource("secrets", secrets));
-        System.out.println("Secret strings are now available at the following environment variables:");
-        secrets.keySet().forEach(key -> System.out.printf("    %s%n", key));
+            System.out.println("Reading secret files:");
+            Map<String, Object> secrets = getSecrets(secretFileNames, secretsFolder);
+            environment.getPropertySources()
+                    .addAfter(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, new MapPropertySource("secrets", secrets));
+            System.out.println("Secret strings are now available at the following environment variables:");
+            secrets.keySet().forEach(key -> System.out.printf("    %s%n", key));
+        } catch (RuntimeException e) {
+            System.out.println(String.format("Stopping file-based secret loading: %s", e.getMessage()));
+        }
+
     }
 
     private String findFolderVar(Map<String, Object> allEnvVariables) {
@@ -72,7 +77,7 @@ public class SecretsLoader implements EnvironmentPostProcessor {
                 .limit(1)
                 .collect(Collectors.toList());
         if (secretsFolderList.isEmpty()) {
-            exit("Error! Secrets folder variable is missing!");
+            throw new RuntimeException("Error! Secrets folder variable is missing!");
         }
         String secretsFolderName = secretsFolderList.get(0);
         System.out.println(String.format("    Secrets folder variable found: %s", secretsFolderName));
@@ -86,7 +91,7 @@ public class SecretsLoader implements EnvironmentPostProcessor {
                 .peek(key -> System.out.printf("    Secret file variable found: %s%n", key))
                 .collect(Collectors.toList());
         if (secretFileEnvVariables.isEmpty()) {
-            exit("Error! No secret file environment variable found!");
+            throw new RuntimeException("Error! No secret file environment variable found!");
         }
         return secretFileEnvVariables;
     }
@@ -123,15 +128,10 @@ public class SecretsLoader implements EnvironmentPostProcessor {
             secret = reader.readLine();
             System.out.printf("    Successfully read: %s%n", secretName);
         } catch (IOException e) {
-            exit(String.format("Error loading secret: %s\n%s", secretName, e));
+            throw new RuntimeException(String.format("Error loading secret: %s\n%s", secretName, e));
         }
         return secret;
     }
 
-    private void exit(String msg) {
-        System.err.println(msg);
-        System.err.println("Exiting application.");
-        System.exit(1);
-    }
 
 }
